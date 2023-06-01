0f2a8fa853dae6eb9b374ee9146beb8574cdc419
y2gon2 <y2gon2@gmail.com>
2023-06-01 20:46:23 +0900

    updated : boj_1920.rs

--- baekjoon/data_structures/boj_1920.rs
+++ baekjoon/data_structures/boj_1920.rs
@@ -6,15 +6,41 @@ fn main() {
     // input 1st line
     let mut buffer = String::new();
     stdin().read_line(&mut buffer).unwrap();
-    let n = buffer.trim().parse::<usize>().unwrap();
+    let _n = buffer.trim().parse::<usize>().unwrap();
 
     // input 2nd line
     buffer.clear();
     stdin().read_line(&mut buffer).unwrap();
-    let numN = buffer
+    let num_n: Vec<usize> = buffer
         .split_ascii_whitespace()
-        .map(|s|)
+        .map(|s| s.parse::<usize>().unwrap())
+        .collect();
 
+    // input 3rd line
+    buffer.clear();
+    stdin().read_line(&mut buffer).unwrap();
+    let _m = buffer.trim().parse::<usize>().unwrap();
 
+    // input 4th line
+    buffer.clear();
+    stdin().read_line(&mut buffer).unwrap();
+    let num_m: Vec<usize> = buffer
+        .split_ascii_whitespace()
+        .map(|s| s.parse::<usize>().unwrap())
+        .collect();
 
+    let mut status = false;
+
+    for i in num_m {
+        for j in num_n.clone() {
+            if i == j {
+                println!("1");
+                status = true;
+                break;
+            }
+        }
+        if !status {
+            println!("0");
+        }
+    }
 }
