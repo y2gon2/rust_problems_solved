9a56c93a4db49ca1fbd04c25b7ddd5c5c8d9248c
y2gon2 <y2gon2@gmail.com>
2023-06-04 21:49:05 +0900

    added: boj_1302.rs

---
+++ baekjoon/data_structures/boj_1302.rs
@@ -0,0 +1,33 @@
+
+// https://www.acmicpc.net/problem/1302
+
+use std::io::*;
+use std::collections::{HashMap, HashSet};
+
+fn main() -> Result<()> {
+    let mut buffer = String::new();
+
+    stdin().read_line(&mut buffer).unwrap();
+    let n = buffer.trim().parse::<usize>().unwrap();  
+
+    let mut books: HashMap<String, usize> = HashMap::new();
+
+    for _ in 0..n {
+        buffer.clear();
+        stdin().read_line(&mut buffer).unwrap();
+        let book = buffer.trim().parse::<String>().unwrap();
+        
+        match books.get_mut(&book) {
+            None => {books.insert(book, 1); ()},
+            Some(n) => { *n += 1; ()},
+        }
+    } 
+
+    let mut books_vec: Vec<_> = books.iter().collect();
+    books_vec.sort_by(|a, b| a.0.cmp(b.0));
+    books_vec.sort_by(|a, b| b.1.cmp(a.1));
+
+    println!("{}", books_vec[0].0);
+
+    Ok(())
+}
