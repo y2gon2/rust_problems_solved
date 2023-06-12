d8eebd9a9a94b202bf3f06b7b6a950182d67209f
y2gon2 <y2gon2@gmail.com>
2023-06-12 11:40:56 +0900

    Added: 배준 19941 햄버거 분배

---
+++ baekjoon/greedy/g08_19941.rs
@@ -0,0 +1,54 @@
+// https://www.acmicpc.net/problem/19941
+// 햄버거 분배
+
+use std::io::{stdin, BufRead, Result, Read};
+use std::cmp::{min, max};
+
+fn main() -> Result<()> {
+    let mut buffer = String::new();
+
+    stdin().lock().read_line(&mut buffer).unwrap();
+    let v: Vec<usize> = buffer
+        .split_ascii_whitespace()
+        .map(|s| s.parse::<usize>().unwrap())
+        .collect();
+
+    let n = v[0];
+    let k = v[1];
+
+    buffer.clear();
+    stdin().lock().read_line(&mut buffer).unwrap();
+    let mut table: Vec<char> = buffer
+        .trim()
+        .chars()
+        .map(|c| c)
+        .collect();
+
+    let mut cnt: usize = 0;
+    for i in 0..n {
+        if table[i] == 'P' {
+            for j in (1..=k) {
+                if i + j < n && table[i + j] == 'H' {
+                    cnt += 1;
+                    table[i] = 'X';
+                    table[i + j] = 'X';
+                    break;
+                } 
+            }
+        } else if table[i] == 'H' {
+            for j in (1..=k) {
+                if i + j < n && table[i + j] == 'P' {
+                    cnt += 1;
+                    table[i] = 'X';
+                    table[i + j] = 'X';
+                    break;
+                } 
+            }
+        }        
+    }
+
+    println!("{}", cnt);
+
+    Ok(())
+}
+
