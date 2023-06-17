d237cf417e6aaf3f1d0c95690a9c77000c00ebd4
y2gon2 <y2gon2@gmail.com>
2023-06-17 13:36:30 +0900

    Added: 백준 1198 삼각형으로자르기

--- baekjoon/geometry/geo07_1198.rs
+++ baekjoon/geometry/geo07_1198.rs
@@ -6,28 +6,39 @@ use std::io::{Result, stdin, Write, BufRead, BufWriter, stdout};
 fn main() -> Result<()> {
     let mut input = stdin().lock().lines().map(|line| line.unwrap());
     let mut output = BufWriter::new(stdout().lock());
-    let mut mx_area: f64 = 0.0;
+    let mut mx_area: i64 = 0;
 
-    let n = input.next().unwrap().parse::<usize>().unwrap();
+    let n = input.next().unwrap().parse::<i64>().unwrap();
     let mut line = || input
         .next()
         .unwrap()
         .split_ascii_whitespace()
-        .map(|s| s.parse::<usize>().unwrap())
-        .collect::<Vec<usize>>();
+        .map(|s| s.parse::<i64>().unwrap())
+        .collect::<Vec<i64>>();
     
-    let mut points: Vec<Vec<usize>> = Vec::new();
+    let mut points: Vec<Vec<i64>> = Vec::new();
 
     for _ in 0..n {
         points.push(line());
     }
 
-    points.push(points[0]);
-    points.push(points[1]);
-
-    for i in 0..n {
-        for j in 
+    // 삼각형 세점의 좌표를 알때 넓이 구하기
+    // s = 1/2 * |(x1y2 + x2y3 + x3y1) - (x1y3 + x3y2 + x2y1)|
+    for i in 0..n as usize{
+        for j in (i + 1)..n as usize{
+            for k in (j + 1)..n as usize {
+                let sq = (
+                    (points[i][0] * points[j][1] + points[j][0] * points[k][1] + points[k][0] * points[i][1])
+                    - (points[j][0] * points[i][1] + points[k][0] * points[j][1] + points[i][0] * points[k][1])                
+                ).abs();
+    
+                mx_area = mx_area.max(sq);
+            }
+        }
     }
+
+    if mx_area % 2 == 1 {println!("{}.5", mx_area / 2 ); }
+    else { println!("{:.1}", mx_area / 2);} 
 
     Ok(())
 }
--- baekjoon/test.rs
+++ baekjoon/test.rs
@@ -1,11 +1,5 @@
-use std::collections::HashMap;
-use std::cmp::max;
+
 
 fn main() {
-    println!("{:?}", most_common_word("Bob hit a ball, the hit BALL flew far after it was hit.".to_string(), vec!["hit".to_string()]));
-
-}
-pub fn most_common_word(paragraph: String, banned: Vec<String>) -> String {
- 
-    return "aa".to_string();
+    println!("{}", (-5.pow(2) as f32).sqrt());
 }
