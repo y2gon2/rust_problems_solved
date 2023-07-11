e32573c669443a3eac9963fada9045157791695a
y2gon2 <y2gon2@gmail.com>
2023-07-11 15:27:52 +0900

    Solved: 백준 1987 알파벳

--- baekjoon/graph/dfs04_1987.rs
+++ baekjoon/graph/dfs04_1987.rs
@@ -1,82 +1,110 @@
 //! https://www.acmicpc.net/problem/1987
 //! 알파벳
 //! 
+//! 풀이과정
+//! DFS 풀이에 재귀가 아닌 stack 상 해당 포인트 까지 온  경로를 같이 저장하는 방식으로 구현했을때,
+//! 시간 초과 발생   
+//! 최초 재귀 구현 : 13164KB / 588ms
+//! 
+//! 주어지는 matrix 를 이중 vec 이 아닌 제한 size 로 고정된 array 로 수정
+//! -> 13156KB / 492ms
+//! 
+//! rust 의 ownership 문제 때문에 재귀함수 구현에 RC<RefCell<_>> 을 고려하였으나,
+//! 순수 (mutable & immutable) reference type paremeter 만으로 구현 가능.
+//! 
 
-use std::io::{stdin, stdout, Write, Read, BufRead};
+use std::io::{stdin, stdout, Write, Read};
 use std::error::Error;
-use std::collections::VecDeque;
+use std::cmp::max;
 
-fn get_nums() -> Result<(usize, usize), Box<dyn Error>> {
+const MAX: usize = 20;
+const DIRECTIONS: [(i32, i32); 4] = [(1, 0), (0, 1), (-1, 0), (0, -1)];
+
+fn get_input() -> Result<(i32, i32, [[char; 20]; 20]), Box<dyn Error>> {
+    let mut board = [[0 as char; MAX]; MAX];
     let mut buf = String::new();
-    let _ = stdin().lock().read_line(&mut buf)?;
+    let _ = stdin().lock().read_to_string(&mut buf)?;
 
-    let info: Vec<usize> = buf
-        .split_ascii_whitespace()
-        .map(|s| s.parse::<usize>())
-        .collect::<Result<_, _>>()
-        .map_err(|e| e)?;  
+    let mut buf_iter = buf
+        .split_ascii_whitespace();
 
-    Ok((info[0], info[1]))
-}
+    let (r, c)  = (
+        buf_iter.next().unwrap().parse::<i32>()?,
+        buf_iter.next().unwrap().parse::<i32>()?
+    );
 
-fn get_board() -> Result<Vec<Vec<char>>, Box<dyn Error>> {
-    let mut buf = String::new();
-    let _ = stdin().lock().read_to_string(&mut buf);
-
-    let board: Vec<Vec<char>> = buf
-        .split_ascii_whitespace()
-        .map(|s| {
-            s.chars().map(|c| c).collect::<Vec<char>>()
-        })
-        .collect::<Vec<Vec<char>>>();
-    
-    Ok(board)
-}
-
-fn dfs(board: Vec<Vec<char>>, r: usize, c: usize) -> Result<usize, Box<dyn Error>> {
-    let mut visited = vec![vec![0u8; c]; r];
-    let directions = [(1, 0), (0, 1), (-1, 0), (0, -1)];
-    let mut aphabets = Vec::<char>::new();
-    let mut stack = Vec::<(usize, usize)>::new(); // y, x
-
-    stack.push((0, 0));
-    visited[0][0] = 1;
-    aphabets.push(board[0][0]);
-    while let Some((py, px)) = stack.pop() {
-        
-        for (dy, dx) in directions {
-            let y = dy + py as i32;
-            let x = dx + px as i32;
-
-            if y >= 0 && x >= 0 && y < r as i32 && x < c as i32 {
-                let y = y as usize;
-                let x = x as usize;
-                
-                if aphabets.contains(&board[y][x]) { continue; }
-                
-                visited[y][x] = visited[py][px] + 1;
-                // aphabets.push(board[y][x]);
-                stack.push((y, x));
-            }
+    for i in 0..r as usize {
+        for (j, ch) in buf_iter.next().unwrap().char_indices() {
+            board[i][j] = ch;
         }
     }
 
-    let mut result = 0usize;
-    for i in 0..r {
-        for j in 0..c {
-            result = result.max(visited[i][j]);
+    Ok((r, c, board))
+}
+
+fn dfs(
+    board: &[[char; 20]; 20], 
+    visited: &mut [bool; 26], 
+    r: i32, 
+    c: i32,
+    cnt: usize,
+    y: i32,
+    x: i32,
+    result: &mut usize
+) {
+    *result = max(*result, cnt);
+    // println!("cnt:{}, result:{}", cnt, result);
+
+    for (dy, dx) in DIRECTIONS.iter() {
+        let ny = y + *dy;
+        let nx = x + *dx;
+
+        if ny >= 0 && nx >= 0 && ny < r  && nx < c {
+            let ny = ny as usize;
+            let nx = nx as usize; 
+            
+            let index = (board[ny][nx] as u8 - b'A') as usize;
+            if visited[index] { continue; }
+
+            visited[index] = true;
+
+            dfs(
+                board,
+                visited,
+                r,
+                c,
+                cnt + 1,
+                ny as i32,
+                nx as i32,
+                result,
+            );
+
+            visited[index] = false;
         }
     }
-    Ok(result)
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut output = stdout().lock();
 
-    let (r, c) = get_nums()?;
-    
-    let mut board: Vec<Vec<char>> = get_board()?;
+    let (r, c, board) = get_input()?;
+    let mut visited = [false; 26];
 
-    writeln!(output, "{}", dfs(board, r, c)?)?;
+    visited[(board[0][0] as u8 - b'A') as usize] = true;
+
+    let mut result = 1usize;
+    
+    dfs(
+        &board, 
+        &mut visited, 
+        r, 
+        c, 
+        1, 
+        0, 
+        0,
+        &mut result
+    );
+
+    writeln!(output, "{}", result)?;
     Ok(())
 }
