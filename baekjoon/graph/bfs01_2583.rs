d01bf43e7cd5213701c149aaa2a0b5a42fad5b5d
y2gon2 <y2gon2@gmail.com>
2023-07-17 10:28:27 +0900

    Resolved: 2583 영역구하기

--- baekjoon/graph/bfs01_2583.rs
+++ baekjoon/graph/bfs01_2583.rs
@@ -1,84 +1,91 @@
-// https://www.acmicpc.net/problem/2583
-// 영역 구하기
+//! https://www.acmicpc.net/problem/2583
+//! 영역 구하기
 
-use std::io::{stdin, stdout, BufWriter, Write, BufRead};
+use std::io::{stdin, stdout, Write, BufRead};
 use std::error::Error;
+use std::collections::VecDeque;
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let mut output = BufWriter::new(stdout().lock());
-    
-    let mut input = stdin().lock().lines().map(|line| line);
+const DIRECTIONS: [(i32, i32); 4] = [(1, 0), (0, 1), (-1, 0), (0, -1)];
 
-    let mut line = || -> Result<Vec<usize>, Box<dyn Error>> {
-        match input.next() {
-            Some(Ok(s)) => {
-                let nums: Result<Vec<usize>, _> = s
-                    .split_ascii_whitespace()
-                    .map(|s| s.parse::<usize>())
-                    .collect();
-                nums.map_err(|e| e.into())
-            },
-            Some(Err(e)) => Err(e.into()),
-            None => Err("No more input".into()),
-        }
-    };    
-
-    let first = line()?;
-    let (n, m, k) = (first[0], first[1], first[2]);
- 
-    let mut sq = vec![vec![0usize; 4]; k];
-    for i in 0..k {
-        sq[i] = line()?;
-
-    }
-
-    let mut matrix = vec![vec![true; m]; n];
-    for s in sq.iter() {
-        for i in s[1]..s[3] {
-            for j in s[0]..s[2] {
-                matrix[i][j] = false;
-            }
-        }
-    }
-
-    // for i in 0..n{
-    //     println!("{:?}", matrix[i]);
-    // }
-
-    let mut rooms = Vec::<usize>::new();
-    let directions = [(1, 0), (0, 1), (-1, 0), (0, -1)];
-
+fn bfs(
+    matrix: &mut Vec<Vec<bool>>,
+    result: &mut Vec<usize>,
+    n: usize,
+    m: usize 
+) -> usize {
+    let mut cnt = 0usize;
     for i in 0..n {
         for j in 0..m {
-            if matrix[i][j] {
-                let mut stack = vec![(i, j)];
-                let mut cnt: usize = 0;
+            if matrix[i][j] { continue; }
 
-                while let Some(p) = stack.pop() {
-                    matrix[p.0][p.1] = false;
-                    cnt += 1;
+            cnt += 1;
+            let mut sub_sq = 1usize;
+            let mut queue = VecDeque::<(usize, usize)>::new();
 
-                    for d in directions.iter() {
-                        let y = p.0 as i32 + d.0;
-                        let x = p.1 as i32 + d.1;
-                        if y >= 0 && x >= 0 && y < n as i32 && x < m as i32 {
-                            if matrix[y as usize][x as usize] && !stack.contains(&(y as usize, x as usize)) {
-                                stack.push((y as usize, x as usize));        
-                            }
-                        } 
+            queue.push_back((i, j));
+            matrix[i][j] = true;
+
+            while let Some((cy, cx)) = queue.pop_front() {
+                for (dy, dx) in DIRECTIONS {
+                    let y = cy as i32 + dy;
+                    let x = cx as i32 + dx;
+                    
+                    if y >= 0 && x >= 0 && y < n as i32 && x < m as i32 {
+                        let y = y as usize;
+                        let x = x as usize;
+
+                        if matrix[y][x] { continue; }
+                        
+                        queue.push_back((y, x));
+                        sub_sq += 1;
+                        matrix[y][x] = true;
                     }
-                    // println!("{:?}", stack);
                 }
-                rooms.push(cnt);
             }
+            result.push(sub_sq);
         }
     }
-    
-    rooms.sort();
-    writeln!(output, "{}", rooms.len())?;
-    for r in rooms.iter() {
-        write!(output, "{} ", r)?;
-    }
+    cnt
+}
 
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut output = stdout().lock();
+    let mut lines = stdin().lock().lines();
+
+    let mut get_nums = || lines
+        .next()
+        .unwrap()
+        .unwrap()
+        .split_ascii_whitespace()
+        .map(|s| s.parse::<usize>())
+        .collect::<Result<Vec<usize>, _>>()
+        .map_err(|e| e);
+        
+    let info = get_nums()?;
+    let mut matrix = vec![vec![false; info[1]]; info[0]];
+    
+    for _ in 0..info[2] {
+        let sq = get_nums()?;
+
+        for y in sq[1]..sq[3] {
+            for x in sq[0]..sq[2] {
+                if !matrix[y][x] {
+                    matrix[y][x] = true;
+                } 
+            }
+        } 
+    } 
+
+    let mut result = Vec::<usize>::new();
+    let cnt = bfs(&mut matrix, &mut result, info[0], info[1]);
+
+    result.sort();
+    let ans = result
+        .iter()
+        .map(|i| i.to_string())
+        .collect::<Vec<String>>()
+        .join(" ");
+
+    writeln!(output, "{}\n{}", cnt, ans)?;     
     Ok(())
 }
